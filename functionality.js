bba2ff73f04a6f3118e8134cd8ece6eda55e8289
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-06-24 16:23:01 -0300

    add face button funcionality

--- functionality.js
+++ functionality.js
@@ -63,63 +63,92 @@ URL.revokeObjectURL(link.href);
 
 
 
+let faceFileInput;
 
+let bodyFileInput;
 
+let reader = [];
 
+let faceContainer = [];
+let bodyContainer = [];
 
+let addButton = document.querySelector('#add');
+addButton.addEventListener('click', changeFaceAndBodyStructure)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-let customFaceInput = document.querySelector('#customFaceInput');
-let customBodyInput = document.querySelector('#customBodyInput');
-
-
-let customFaceInputButton = document.querySelector('#addCustomFacesButton');
-let customBodyInputButton = document.querySelector('#addCustomBodiesButton');
-
-document.querySelector('#customSection').addEventListener('click', customizeGenerator)
-
-function customizeGenerator(event){
-if(event.target == customFaceInputButton){
-alert('you touched the face input');
-changeFacesArray();
-}
-else if(event.target == customBodyInputButton){
-alert('you touched the body input');
-}
-}
-
-let newArray2;
-function changeFacesArray(){
-let newArray2 = customFaceInput.value.split(',,');
-let newArray3 = newArray2.map(word => word.replace(/[\n\t]/g, ''));
-
-// to-doadd a for loop that parses all of the items and make it an object
-//newArray = newArray3.map(word => JSON.parse(word) );
-
-document.querySelector('#faceSelect').innerHTML = '';
-for(let index = 0;newArray.length > index; index++){
-newOption = document.createElement('option');
-newOption.value = newArray[index].name;
-newOption.innerText = newArray[index].name;
-document.querySelector('#faceSelect').appendChild(newOption);
+function changeFaceAndBodyStructure(){
+faces = [];
+changeFace();
+//changeBody();
 }
 
 
-faces = newArray;
 
-
+function changeFace(){
+faceFileInput = document.querySelector('#faceFileInput');
+addUploadedInformationToTheselectedContainer(faceFileInput, faceContainer);
+faceSeperator(faceContainer)
 
 }
 
+
+
+
+//function changeBody(){
+//bodyFileInput = document.querySelector('#bodyFileInput');
+//addUploadedInformationToTheselectedContainer(bodyFileInput, bodyContainer);
+//}
+
+variable2 = [];
+
+function addUploadedInformationToTheselectedContainer(container, container2){
+for(let index = 0;container.files.length > index;index++){
+reader[index] = new FileReader();
+reader[index].readAsText(container.files[index]);
+reader[index].onload = function() {
+container2[index] = reader[index].result;
+};}}
+
+
+
+let svge1;
+let svge2;
+
+
+let var1;
+let var2;
+let var3;
+let var4;
+let nameForFace;
+let nameForFace2;
+
+function faceSeperator(variable){
+
+for(let index in variable){
+
+variable2[index] = variable[index].split('');
+
+for(let index2 = 0; variable2[index].length > index2;index2++){
+if(variable2[index][index2] == '<' && variable2[index][index2 + 1] == 'd'){
+var1= index2;
+}
+else if(variable2[index][index2] == '<' && variable2[index][index2 + 1] == '/' && variable2[index][index2 + 2] == 'd'){
+var2= index2 + 6;
+}
+else if(variable2[index][index2] == '<' && variable2[index][index2 + 1] == 'u'){
+var3= index2;
+}
+else if(variable2[index][index2] == '>' && variable2[index][index2 + 1] == '\n' && variable2[index][index2 + 2] == '<' && variable2[index][index2 + 3] == 's' && variable2[index][index2 + 4] == 'v'){
+var4= index2+ 2;
+}
+
+svge1 = variable2[index].slice(var1, var2).join('');
+svge2 = variable2[index].slice(var3, var4).join('');
+
+nameForFace = faceFileInput.files[index].name;
+nameForFace2 = nameForFace.substring(0,nameForFace.length -4);
+
+faces[index] = {name:`${nameForFace2}`, svg1: `${svge1}`, svg2: `${svge2}`};
+}
+
+}
+}
